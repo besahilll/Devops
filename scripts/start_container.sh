7c1ea53713d6337343a5eaf576eee13f1a3e97ff
Sahil Bathla <99262172+besahilll@users.noreply.github.com>
2025-08-17 19:20:48 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull besahil/simple-python-flask-app:latest
+docker pull besahil/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 besahil/simple-python-flask-app:latest
+docker run -d -p 5000:5000 besahil/simple-python-flask-app
  
